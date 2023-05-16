41a5e5196a5487ea8c881cdbf70d5bd1aec23619
Michael Do <112127345+MycoalDough@users.noreply.github.com>
2023-05-16 13:41:33 -0700

    Create Bacteria_RLAgent01.cs

---
+++ Bacteria_RLAgent01.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class HelloWorld
+{
+  public static void Main(string[args])
+  {
+    int bacteriaID;
+    OnCollisionEnter(Collider col)
+    {
+      if(col.gameObject.tag == "Prey")
+      {
+        
+      }
+    }
+  }
+}
