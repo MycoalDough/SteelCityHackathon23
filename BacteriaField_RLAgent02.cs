34cf37e1b756f3d358d706c039bc69b37a465708
Michael Do <112127345+MycoalDough@users.noreply.github.com>
2023-05-17 13:54:59 -0700

    Update BacteriaField_RLAgent02.cs

--- BacteriaField_RLAgent02.cs
+++ BacteriaField_RLAgent02.cs
@@ -1,3 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BacteriaAI : MonoBehaviour
+{
+
+    [Header("Genes")]
     public float reproductionRate;
     private float rRCT;
     public int ID; //used for memory cells 
@@ -7,13 +15,39 @@
     private Vector3 desiredPos;
     public float moveDir = 5;
     private float yPos;
+    public bool replicating;
 
     [Header("Mutations")]
     public bool cough;
     public bool TOF;
     public bool fatigue;
     public bool SL; //surface livibility
+    public bool BI; //birth infection
+    // Start is called before the first frame update
+    void Start()
+    {
+        yPos = 1.2f;
+        desiredPos = new Vector3(transform.position.x + Random.Range(-moveDir, moveDir), yPos, transform.position.z + Random.Range(-moveDir, moveDir));
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, desiredPos, speed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, desiredPos) > 0.5f) return;
+
+        desiredPos = new Vector3(transform.position.x + Random.Range(-moveDir, moveDir), yPos, transform.position.z + Random.Range(-moveDir, moveDir));
+        transform.LookAt(desiredPos);
+        transform.eulerAngles = new Vector3(90f, transform.rotation.y, transform.rotation.z);
+    }
     
-    public Bacteria_RLAgent01 ba;
+    void OnTriggerEnter(Collider col){
+        if(col.gameObject.tag == "HostCell")
+        {}
+    
+    }
     
     
+}
