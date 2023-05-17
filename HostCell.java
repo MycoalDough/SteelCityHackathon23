c4c3dfb6ca2d488c8e84947fd0a0d32c1245f661
Michael Do <112127345+MycoalDough@users.noreply.github.com>
2023-05-17 15:03:47 -0700

    Update HostCell.java

--- HostCell.java
+++ HostCell.java
@@ -4,10 +4,25 @@ using UnityEngine;
 
 public class HostCell : Prey_AI
 {
+    public Microspace ms;
     [Header("Values")]
     public bool controlled;
     public int bacteriaInside = 0;
     public int maxBacteria = 20;
+    public List<BacteriaField_RLAgent02> insides = new List<BacteriaField_RLAgent02>();
+    private int relicationTime = 30;
+    private float repTimer = 0.00f;
+    
+    public void Update(){
+        if(bacteriaInside >= maxBacteria){
+            for(int i = 0; i < insides.Count; i++)
+            {
+                insides.get(i).outOfCell = true;
+            }
+            ms.numCells--;
+            Destroy(gameObject);
+        }
+    }
     
     
 }
