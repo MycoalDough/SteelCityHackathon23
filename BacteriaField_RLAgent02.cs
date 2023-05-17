0a06478457c037b507100db039eeac125e148bb4
Michael Do <112127345+MycoalDough@users.noreply.github.com>
2023-05-17 13:48:22 -0700

    Create BacteriaField_RLAgent02.cs

---
+++ BacteriaField_RLAgent02.cs
@@ -0,0 +1,19 @@
+    public float reproductionRate;
+    private float rRCT;
+    public int ID; //used for memory cells 
+    public float detection;
+    public float healthOutside;
+    public float speed;
+    private Vector3 desiredPos;
+    public float moveDir = 5;
+    private float yPos;
+
+    [Header("Mutations")]
+    public bool cough;
+    public bool TOF;
+    public bool fatigue;
+    public bool SL; //surface livibility
+    
+    public Bacteria_RLAgent01 ba;
+    
+    
