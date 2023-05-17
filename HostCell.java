652a6cdeb25ebac744317228263c741ef2900d6a
Michael Do <112127345+MycoalDough@users.noreply.github.com>
2023-05-17 14:16:41 -0700

    Update and rename HostCell to HostCell.java

---
+++ HostCell.java
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HostCell : Prey_AI
+{
+    [Header("Values")]
+    public bool controlled;
+    public int bacteriaInside = 0;
+    public int maxBacteria = 20;
+    
+    
+}
