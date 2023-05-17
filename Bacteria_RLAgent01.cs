33308a3583d406c2c0db5bfedabdeeb1d23d8f7a
Michael Do <112127345+MycoalDough@users.noreply.github.com>
2023-05-17 07:48:12 -0700

    Update Bacteria_RLAgent01.cs

--- Bacteria_RLAgent01.cs
+++ Bacteria_RLAgent01.cs
@@ -1,16 +1,44 @@
-using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-public class HelloWorld
+public class BacteriaAI : MonoBehaviour
 {
-  public static void Main(string[args])
-  {
-    int bacteriaID;
-    OnCollisionEnter(Collider col)
+
+    [Header("Genes")]
+    public float reproductionRate;
+    private float rRCT;
+    public int ID; //used for memory cells 
+    public float detection;
+    public float healthOutside;
+    public float speed;
+    private Vector3 desiredPos;
+    public float moveDir = 5;
+    private float yPos;
+
+    [Header("Mutations")]
+    public bool cough;
+    public bool TOF;
+    public bool fatigue;
+    public bool SL; //surface livibility
+    public bool BI; //birth infection
+    // Start is called before the first frame update
+    void Start()
     {
-      if(col.gameObject.tag == "Prey")
-      {
-        
-      }
+        yPos = 1.2f;
+        desiredPos = new Vector3(transform.position.x + Random.Range(-moveDir, moveDir), yPos, transform.position.z + Random.Range(-moveDir, moveDir));
+
     }
-  }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, desiredPos, speed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, desiredPos) > 0.5f) return;
+
+        desiredPos = new Vector3(transform.position.x + Random.Range(-moveDir, moveDir), yPos, transform.position.z + Random.Range(-moveDir, moveDir));
+        transform.LookAt(desiredPos);
+        transform.eulerAngles = new Vector3(90f, transform.rotation.y, transform.rotation.z);
+    }
 }
