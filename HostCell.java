b03730d328045ebeae20fea5d97ad7af275cd41a
Michael Do <112127345+MycoalDough@users.noreply.github.com>
2023-05-18 07:54:08 -0700

    Update HostCell.java

--- HostCell.java
+++ HostCell.java
@@ -2,27 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HostCell : Prey_AI
+public class HostCell : MonoBehaviour
 {
     public Microspace ms;
     [Header("Values")]
+    public bool canReplicate = false;
     public bool controlled;
     public int bacteriaInside = 0;
     public int maxBacteria = 20;
     public List<BacteriaField_RLAgent02> insides = new List<BacteriaField_RLAgent02>();
     private int relicationTime = 30;
     private float repTimer = 0.00f;
-    
-    public void Update(){
-        if(bacteriaInside >= maxBacteria){
-            for(int i = 0; i < insides.Count; i++)
+
+    public void Update()
+    {
+        if (bacteriaInside >= maxBacteria)
+        {
+            for (int i = 0; i < insides.Count; i++)
             {
-                insides.get(i).outOfCell = true;
+                insides[i].replicating = false;
             }
             ms.numCells--;
             Destroy(gameObject);
         }
     }
-    
-    
 }
