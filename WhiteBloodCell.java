b3d476a0374e3cdc525eef0f783ca8583b8005e8
Michael Do <112127345+MycoalDough@users.noreply.github.com>
2023-05-18 13:43:00 -0700

    Update WhiteBloodCell.java

--- WhiteBloodCell.java
+++ WhiteBloodCell.java
@@ -1,1 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
+public class Microspace : MonoBehaviour
+{
+  public float lifeTime = 10;
+  public float currentLife = 0;
+  
+  public void Update()
+  {
+  
+  }
+}
